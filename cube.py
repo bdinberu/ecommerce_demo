f0c3103e61b653ac03841ecc769def4d2ccd1630
bruk <bruk@cube.dev>
2024-12-03 22:29:19 +0000

    added semantic layer sync for Tableau

--- cube.py
+++ cube.py
@@ -1,3 +1,18 @@
 # Cube configuration options: https://cube.dev/docs/config
 
 from cube import config
+
+
+@config('semantic_layer_sync')
+def sls(ctx: dict) -> list:
+    return [{
+  "type": "tableau-cloud",
+  "name": "Tableau Cloud Sync",
+  "config": {
+    "database": "Cube Cloud: ecomm_bd",
+    "personalAccessToken": "ecomm_demo_bd",
+    "personalAccessTokenSecret": "CaSfFFyOR3mj1ncwLQHAww==:8jUMFFHdlhXSdcbdH6VZFzxHfQKZPsrL",
+    "region": "us-west-2b",
+    "site": "cubedev"
+  }
+}]
