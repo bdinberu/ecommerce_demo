5c310754561b876096b7d83c0e19389e46eca7a8
bruk <bruk@cube.dev>
2024-12-05 16:43:15 +0000

    added a new function to parse contentToRole filter object update and ensure that queries have a filter applied before executing

--- cube.py
+++ cube.py
@@ -17,9 +17,11 @@ def sls(ctx: dict) -> list:
   }
 }]
 
+
 @config('context_to_app_id')
 def context_mapping(ctx: dict):
   return ctx['securityContext'].setdefault('roles')
+
 
 # Identifying any roles that have been assigned to the user 
 @config('context_to_roles')
@@ -31,15 +33,37 @@ def context_to_roles(context):
         print("No roles found, returning empty list.")
     return roles
 
-# Content to Rules modifies the filter object and adds a few blank placeholders. 
-# Creating a function to only return the actual filter conditions 
 
+# ContentToRules modifies the filter object and adds a new 'and' dictionary 
+# Determining the actual filter conditions 
+def extract_matching_dicts(data):
+    matching_dicts = []
+    keys = ['values', 'member', 'operator']
+
+    # Recursive function to traverse through the list or dictionary
+    def traverse(element):
+        if isinstance(element, dict):
+            # Check if any of the specified keys are in the dictionary
+            if any(key in element for key in keys):
+                matching_dicts.append(element)
+            # Traverse the dictionary values
+            for value in element.values():
+                traverse(value)
+        elif isinstance(element, list):
+            # Traverse the list items
+            for item in element:
+                traverse(item)
+
+    traverse(data)
+    return matching_dicts
 
 
 # Creating a data policy that restricts queries without filters from being executed
 @config('query_rewrite')
 def query_rewrite(query: dict, ctx: dict) -> dict:
   print(query.get('filters'))
-  if not query.get('filters', []):
+  filters = extract_matching_dicts(query.get('filters'))
+  
+  if not filters:
     raise Exception("Queries can't be run without a filter")
   return query 
